89a4c9cbec51e11eb8521683613355f6e26cad0c
DESKTOP-3HAS177\rusni <rus.nigmat@gmail.com>
2022-10-10 02:52:04 +0700

    TODO: Поправить condition

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,20 +1,26 @@
+import java.util.concurrent.atomic.AtomicInteger;
 import java.util.concurrent.locks.Condition;
 import java.util.concurrent.locks.ReentrantLock;
 
 public class Main {
+    private static final int COUNT_OF_CLIENTS = 5;
+    private static final int COUNT_OF_WAITERS = 2;
+    private static final int COUNT_OF_COOKS = 1;
     static ReentrantLock locker = new ReentrantLock();
     static Condition condition = locker.newCondition();
     static Condition orderCondition = locker.newCondition();
     static Condition startCookCondition = locker.newCondition();
     static Condition stopCookCookCondition = locker.newCondition();
     static Condition letEatCondition = locker.newCondition();
+    static int countOfServed = 0;
+
     public static void main(String[] args) {
         Restaurant restaurant = new Restaurant();
-        Runnable clientBehavior = () ->{
+        Runnable clientBehavior = () -> {
             locker.lock();
             String name = Thread.currentThread().getName();
             System.out.println(name + " come in");
-            if (!restaurant.hasWaiter()){
+            if (!restaurant.hasWaiter()) {
                 try {
                     System.out.println("Wait for available waiter");
                     condition.await();
@@ -33,55 +39,69 @@ public class Main {
             }
             System.out.println("eat");
             System.out.println("come out");
+            countOfServed++;
             locker.unlock();
         };
 
-        new Thread(clientBehavior, "Client 01").start();
+        for (int i = 0; i < COUNT_OF_CLIENTS; i++) {
+            new Thread(clientBehavior, "Client 0" + i).start();
+        }
 
         Runnable waiterBehavior = () -> {
-            locker.lock();
-            System.out.println("Waiter on work");
-            restaurant.waiterOnWork();
-            condition.signal();
 
-            if(!restaurant.hasOrder()) {
+            while (countOfServed < COUNT_OF_CLIENTS) {
+                locker.lock();
+                System.out.println("Waiter on work");
+                restaurant.waiterOnWork();
+                condition.signal();
+
+                if (!restaurant.hasOrder()) {
+                    try {
+                        orderCondition.await();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+                System.out.println("Get order");
+                startCookCondition.signal();
                 try {
-                    orderCondition.await();
+                    stopCookCookCondition.await();
                 } catch (InterruptedException e) {
                     throw new RuntimeException(e);
                 }
+                System.out.println("Bring the dish");
+                letEatCondition.signal();
+                locker.unlock();
             }
-            System.out.println("Get order");
-            startCookCondition.signal();
-            try {
-                stopCookCookCondition.await();
-            } catch (InterruptedException e) {
-                throw new RuntimeException(e);
-            }
-            System.out.println("Bring the dish");
-            letEatCondition.signal();
-            locker.unlock();
+
         };
 
-        new Thread(waiterBehavior, "Waiter 01").start();
+        for (int i = 0; i < COUNT_OF_WAITERS; i++) {
+            new Thread(waiterBehavior, "Waiter 01").start();
+        }
 
-        Runnable cookBehavior = () ->{
-            locker.lock();
-            System.out.println("Cook on work");
-            if(!restaurant.hasOrder()){
-                System.out.println("Wait orders");
-                try {
-                    startCookCondition.await();
-                } catch (InterruptedException e) {
-                    throw new RuntimeException(e);
+        Runnable cookBehavior = () -> {
+
+            while (countOfServed < COUNT_OF_CLIENTS) {
+                locker.lock();
+                System.out.println("Cook on work");
+                if (!restaurant.hasOrder()) {
+                    System.out.println("Wait orders");
+                    try {
+                        startCookCondition.await();
+                    } catch (InterruptedException e) {
+                        throw new RuntimeException(e);
+                    }
                 }
+                System.out.println("Start cooking");
+                stopCookCookCondition.signal();
+                System.out.println("Stop cooking");
+                locker.unlock();
             }
-            System.out.println("Start cooking");
-            stopCookCookCondition.signal();
-            System.out.println("Stop cooking");
-            locker.unlock();
-        };
 
-        new Thread(cookBehavior, "Cook 01").start();
+        };
+        for (int i = 0; i < COUNT_OF_COOKS; i++) {
+            new Thread(cookBehavior, "Cook 01").start();
+        }
     }
 }
